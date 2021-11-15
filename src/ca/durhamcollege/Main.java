7df5bc1cf3fb929eb22a3eb7c436a42b67f37f8a
soumyann <soumya.narayanamandiramnarayan@dcmail.ca>
2021-11-14 22:18:39 -0500

    Created function to get user input.

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -8,6 +8,29 @@ public class Main {
     private static String PLAYER_2 = "Shweta";
     private static String PLAYER_3 = "Tom";
 
+    private static String getScore(String name, int scoreNo)
+    {
+        Scanner myObj = new Scanner(System.in); // Creating Scanner object
+        String score = null;
+        while (score==null)
+        {
+            System.out.printf("Please enter " + name + "'s Score #"+scoreNo+": ");
+            score = myObj.nextLine();
+            try{
+                int temp = Integer.parseInt(score);
+                if(!(temp>=0 && temp <=300)) {
+                    System.out.println("Invalid input. Value between 0 and 300 needed. Please try again");
+                    score = null;
+                }
+            }
+            catch (NumberFormatException ex){
+                System.out.println("Invalid input. Numeric value needed. Please try again");
+                score = null;
+            }
+        }
+        return score;
+    }
+
     public static void main(String[] args)
     {
         String game[][] = new String[3][3];
@@ -15,49 +38,10 @@ public class Main {
         game[1][0] = PLAYER_2;
         game[2][0] = PLAYER_3;
 
-        Scanner myObj = new Scanner(System.in); // Creating Scanner object
         for (int i=0; i<3;i++)
         {
-            String scoreOne = null;
-            String scoreTwo = null;
-
-            while (scoreOne==null)
-            {
-                System.out.printf("Please enter " + game[i][0] + "'s Score #1: ");
-                scoreOne = myObj.nextLine();
-                try{
-                    int temp = Integer.parseInt(scoreOne);
-                    if(!(temp>=0 && temp <=300)) {
-                        System.out.println("Invalid input. Value between 0 and 300 needed. Please try again");
-                        scoreOne = null;
-                    }
-                }
-                catch (NumberFormatException ex){
-                    System.out.println("Invalid input. Numeric value needed. Please try again");
-                    scoreOne = null;
-                }
-            }
-            game[i][1] = scoreOne;
-
-
-            while (scoreTwo==null)
-            {
-                System.out.printf("Please enter "+ game[i][0]+"'s Score #2: ");
-                scoreTwo = myObj.nextLine();
-
-                try{
-                    int temp = Integer.parseInt(scoreTwo);
-                    if(!(temp>=0 && temp <=300)) {
-                        System.out.println("Invalid input. Value between 0 and 300 needed. Please try again");
-                        scoreTwo = null;
-                    }
-                }
-                catch (NumberFormatException ex){
-                    System.out.println("Invalid input. Numeric value needed. Please try again");
-                    scoreTwo = null;
-                }
-            }
-            game[i][2]=scoreTwo;
+            game[i][1] = getScore(game[i][0], 1);
+            game[i][2] = getScore(game[i][0], 2);
 
             System.out.println("-------------------------------------------------");
         }
